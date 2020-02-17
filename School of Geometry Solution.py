05b4acb70ab783099d707432cb62e7e1ddc52b12
Sagar Agrawal <52325769+sagagrawal@users.noreply.github.com>
2020-02-17 19:26:22 +0530

    Add files via upload

---
+++ School of Geometry Solution.py
@@ -0,0 +1,30 @@
+Testcases = int(input())
+DiameterTotalArray = list()
+DiameterTotal = 0
+
+for index in range(0, Testcases):
+    NInputs = int(input())
+
+    LengthInput = input()
+    LengthArray = list()
+    LengthArray = [int(i) for i in LengthInput.split(' ')]
+
+    WidthInput = input()
+    WidthArray = list()
+    WidthArray = [int(i) for i in WidthInput.split(' ')]
+
+    LengthArray.sort()
+    WidthArray.sort()
+
+    DiameterTotal = 0
+
+    for i in range(0, NInputs):
+        if LengthArray[i] < WidthArray[i]:
+            DiameterTotal += LengthArray[i]
+        else:
+            DiameterTotal += WidthArray[i]
+
+    DiameterTotalArray.append(DiameterTotal)
+
+for item in DiameterTotalArray:
+    print(item)
